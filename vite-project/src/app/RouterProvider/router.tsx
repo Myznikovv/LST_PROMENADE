af2ee7b7598c6f36e1af864f5d667fba233f13e8
payne3105 <payne3105@gmail.com>
2023-11-05 16:50:38 +0300

    fix (navigation-logic): added logic for bottom bar

--- vite-project/src/app/RouterProvider/router.tsx
+++ vite-project/src/app/RouterProvider/router.tsx
@@ -1,15 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
+import { EErrorTexts } from "../../shared/enums/EErrorTexts";
+
+import LoginPage from "../../pages/Login";
+import Tasks from "../../pages/Tasks";
+import Chat from "../../pages/Chat";
+import Courses from "../../pages/Courses";
 import RootLayout from "../../pages/RootLayout";
 import Error from "../../pages/Error";
-import { EErrorTexts } from "../../shared/enums/EErrorTexts";
-import Box from "@mui/material/Box";
-import LoginPage from "../../pages/Login";
+import Profile from "../../pages/Profile";
 
 export const router = createBrowserRouter([
   {
-    path:"/login",
-    element:<LoginPage/>,
-
+    path: "/login",
+    element: <LoginPage />,
   },
   {
     path: "/",
@@ -17,7 +20,20 @@ export const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Box>Test</Box>,
+        path: "/tasks",
+        element: <Tasks />,
+      },
+      {
+        path: "/chat",
+        element: <Chat />,
+      },
+      {
+        path: "/courses",
+        element: <Courses />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
       },
     ],
     errorElement: <Error errorReason={EErrorTexts.Error404} />,
