5e92d730bcc07f9cacb8ac07cf355ff26c0bd8a8
Vitaly.Myznikov <Vitaly.Myznikov@b1.ru>
2023-11-05 12:10:40 +0300

    feat: change some root layout

--- vite-project/src/pages/Login/index.tsx
+++ vite-project/src/pages/Login/index.tsx
@@ -1,8 +1,13 @@
 import { palette } from "../../shared/config/palette";
+//пока что не понял как полность кастомизировать палитру из-за этого пока что так оставил
 import SovcomBankLogo from "../../shared/components/Icons/SovcomBankLogo";
-import { styled } from "@mui/material";
-import TextField from "@mui/material/TextField";
+import { OutlinedInput, styled } from "@mui/material";
 import Button from "@mui/material/Button";
+import FormControl from "@mui/material/FormControl";
+import FormHelperText from "@mui/material/FormHelperText";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { theme } from "../../app/ThemeProvider/theme";
 
 const LoginLayout = styled("div")({
   background: palette.background.tertiary,
@@ -13,7 +18,7 @@ const LoginLayout = styled("div")({
 });
 
 const LoginForm = styled("div")({
-  background: palette.monochrome.white,
+  background: theme.palette.common.white,
   textAlign: "center",
   width: "38.125rem",
   height: "27.5rem",
@@ -27,18 +32,17 @@ const LoginForm = styled("div")({
 
 const TypographyH1 = styled("h1")({
   fontFamily: "Inter",
-  fontSize: "1.5rem",
+  fontSize: "2rem",
   fontStyle: "normal",
   fontWeight: "700",
   marginBottom: "2.5rem",
 });
 
-const Input = styled(TextField)({
-  width: " 18.75rem",
+const StyledInput = styled(OutlinedInput)({
+  width: " 24.75rem",
   borderRadius: " 0.625rem",
   border: ` 1px solid ${palette.secondary.borderGrey}`,
   background: palette.monochrome.white,
-  marginBottom: "0.63rem",
 });
 
 const StyledButton = styled(Button)({
@@ -46,26 +50,77 @@ const StyledButton = styled(Button)({
   fontFamily: "Inter",
   fontSize: "1.25rem",
   fontStyle: "normal",
-  fontWeight: "500",
+  fontWeight: "600",
   background: palette.button.default,
   color: palette.monochrome.white,
-  width: " 18.75rem",
+  width: " 24.75rem",
   padding: "0.9375rem",
-  marginTop: "0.75rem",
+  marginTop: "1.25rem",
   ":hover": {
     background: palette.button.hover,
   },
 });
 
+const StyledFormHelperText = styled(FormHelperText)({
+  margin: "0.31rem 0 0 0",
+  fontSize: "0.875rem",
+  fontFamily: "Inter",
+});
+
+const StyledFormControl = styled(FormControl)({
+  marginBottom: "0.61rem",
+});
+
 const LoginPage = () => {
+  const navigate = useNavigate();
+  const [error, setError] = useState<boolean>();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordlChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setPassword(event.target.value);
+  };
+
+  const handleEnterClick = () => {
+    navigate("/");
+  };
   return (
     <LoginLayout>
       <LoginForm>
         <SovcomBankLogo />
         <TypographyH1>Вход в Совкомком Визитер</TypographyH1>
-        <Input placeholder="Корпоративная почта"></Input>
-        <Input placeholder="Пароль"></Input>
-        <StyledButton>Войти</StyledButton>
+        <StyledFormControl error={error}>
+          <StyledInput
+            value={email}
+            onChange={handleEmailChange}
+            placeholder="Корпоративная почта"
+          ></StyledInput>
+          {error ? (
+            <StyledFormHelperText id="my-helper-text">
+              Введена неверная почта
+            </StyledFormHelperText>
+          ) : null}
+        </StyledFormControl>
+        <StyledFormControl error={error}>
+          <StyledInput
+            value={password}
+            onChange={handlePasswordlChange}
+            placeholder="Пароль"
+          ></StyledInput>
+          {error ? (
+            <StyledFormHelperText id="my-helper-text">
+              Введён неверный пароль
+            </StyledFormHelperText>
+          ) : null}
+        </StyledFormControl>
+        <StyledButton onClick={handleEnterClick}>Войти</StyledButton>{" "}
+        {/*Пока что здесь будет заглушка и переход сразу на стартовую страницу */}
       </LoginForm>
     </LoginLayout>
   );
--- vite-project/src/pages/RootLayout/index.tsx
+++ vite-project/src/pages/RootLayout/index.tsx
@@ -1,7 +1,3 @@
-import { Suspense } from "react";
-import Box from "@mui/material/Box";
-import { CircularProgress } from "@mui/material";
-import { Outlet } from "react-router-dom";
 import useMediaSize from "../../shared/hooks/useMediaSize";
 import DesktopRootLayout from "./DesktopRootLayout";
 import MobileRootLayout from "./MobileRootLayout";
@@ -10,12 +6,6 @@ export default function RootLayout() {
   const mediaSize = useMediaSize();
   return (
     <>
-      <Box className="drawer" />
-      <Box className="content">
-        <Suspense fallback={<CircularProgress />}>
-          <Outlet />
-        </Suspense>
-      </Box>
       <>{mediaSize.isMobile ? <MobileRootLayout /> : <DesktopRootLayout />}</>
     </>
   );
