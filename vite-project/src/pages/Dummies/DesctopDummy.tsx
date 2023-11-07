41b2418a50b68c0f7a269e140136171cb5b96eb8
Vitaly.Myznikov <Vitaly.Myznikov@b1.ru>
2023-11-07 10:33:14 +0300

    fix: feat add dummy pages not styled

---
+++ vite-project/src/pages/Dummies/DesctopDummy.tsx
@@ -0,0 +1,15 @@
+import Box from "@mui/material/Box";
+import { theme } from "../../app/ThemeProvider/theme";
+
+const DesctopDummy = () => {
+  return (
+    <Box
+      sx={{
+        background: theme.palette.background.default,
+        
+      }}
+    >Здесь будет заглушка</Box>
+  );
+};
+
+export default DesctopDummy;
--- vite-project/src/pages/RootLayout/DesktopRootLayout.tsx
+++ vite-project/src/pages/RootLayout/DesktopRootLayout.tsx
@@ -1,14 +1,21 @@
 import CircularProgress from "@mui/material/CircularProgress";
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
+import DesctopDummy from "../Dummies/DesctopDummy";
 
 export default function DesktopRootLayout() {
+  const permission = "visitor"; //это временная заглушка по пермиссии для пользователя
+
   return (
     <div>
-      <Suspense fallback={<CircularProgress />}>
-        <Outlet />
-      </Suspense>
-      DesktopRootLayout
+      {permission !== "visitor" ? (
+        <Suspense fallback={<CircularProgress />}>
+          <Outlet />
+        </Suspense>
+      ) : (
+        <DesctopDummy />
+      )}
+      
     </div>
   );
 }
