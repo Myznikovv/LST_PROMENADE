70766367da7bfdecd6b2fa53495ab0c9a80f74f4
payne3105 <payne3105@gmail.com>
2023-11-09 00:56:01 +0300

    fix (dashboard): size and position fixes

--- vite-project/src/pages/Tasks/ManagerTaskPage/index.tsx
+++ vite-project/src/pages/Tasks/ManagerTaskPage/index.tsx
@@ -1,9 +1,13 @@
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { DashboardCard } from "../../../shared/components/DashboardCard";
 import { typographyDesktop } from "../../../shared/config/typography";
 import { styled } from "@mui/material";
 
 const DashboardContent = styled(Box)({
+  display: "grid",
+  gridTemplateColumns: "repeat(3, 1fr)",
+  rowGap: "1rem",
+  columnGap: "2rem",
   marginTop: "2.5rem",
 });
 
@@ -12,25 +16,19 @@ const ManagerTaskPage = () => {
     <Box>
       <Typography sx={{ ...typographyDesktop.h1 }}>Задачи</Typography>
       <DashboardContent>
-        <Stack direction="row">
-          <DashboardCard
-            title="Задач запланировано"
-            count={7}
-            date="07.11.23"
-          />
-          <DashboardCard
-            title="Задачи выполнено"
-            count={2}
-            date="08.11.23"
-            color="#CD3636"
-          />
-          <DashboardCard
-            title="Задач не выполнено"
-            count={5}
-            date="07.11.23"
-            color="#2F9461"
-          />
-        </Stack>
+        <DashboardCard title="Задач запланировано" count={7} date="07.11.23" />
+        <DashboardCard
+          title="Задачи выполнено"
+          count={2}
+          date="08.11.23"
+          color="#CD3636"
+        />
+        <DashboardCard
+          title="Задач не выполнено"
+          count={5}
+          date="07.11.23"
+          color="#2F9461"
+        />
       </DashboardContent>
     </Box>
   );
